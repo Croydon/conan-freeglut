dd019e02bab8206309691d668974810b23dc58d5
Croydon <cr0ydon@outlook.com>
2018-09-05 17:48:43 +0200

    Fix linking

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -9,7 +9,7 @@ void error_handler( const char *fmt, va_list ap)
 int main(int argc, char **argv)
 {
     glutInitErrorFunc(error_handler);
-    glutInit();
+    glutInit(&argc, argv);
     std::cout << glutGet(GLUT_VERSION);
     return 0;
 }
