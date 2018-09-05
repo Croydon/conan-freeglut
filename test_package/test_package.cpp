e0b82712ed21c16c459dd58122fb6d128fdf5e5b
Croydon <cr0ydon@outlook.com>
2018-09-05 17:14:29 +0200

    Fix linking

--- test_package/test_package.cpp
+++ test_package/test_package.cpp
@@ -1,17 +1,8 @@
 #include "GL/freeglut.h"
+#include <iostream>
 
 int main(int argc, char **argv)
 {
-    glutInit(&argc, argv);
-    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH);
-    glutInitWindowSize(600, 600);
-    glutCreateWindow("Test package");
-
-    glClearColor(0.5, 0.5, 0.5, 1.0);
-    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
-
-    glLoadIdentity();
-    glutSolidCube(0.1);
-
+    std::cout << glutGet(GLUT_VERSION);
     return 0;
 }
