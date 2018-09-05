234dfc945c9047bc5ff713eba463eeaefa32fd41
Croydon <cr0ydon@outlook.com>
2018-09-05 18:39:11 +0200

    Fix linking

--- conanfile.py
+++ conanfile.py
@@ -163,10 +163,11 @@ class freeglutConan(ConanFile):
                 self.cpp_info.libs.append("GL")
                 self.cpp_info.libs.append("GLU")
 
-        #if self.options.shared:
-            #self.cpp_info.libs.append("libglut")
-        #else:
-            #self.cpp_info.libs.append("freeglut")
+        if self.options.replace_glut:
+            self.cpp_info.libs.append("libglut")
+        else:
+            self.cpp_info.libs.append("freeglut")
 
         self.cpp_info.libs.extend(tools.collect_libs(self))
         self.output.info(tools.collect_libs(self))
+        self.output.info(self.cpp_info.libs)
